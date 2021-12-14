c74b670752659c8b5ebb3fe33982e7d567415e8b
Stefan Cucuta <cucutastefan@gmail.com>
2021-12-14 19:14:58 +0000

    Add user and computer choices and event listenears

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,3 +81,121 @@ function DRAW(userChoice, compChoice) {
 }
 
 
+// Score Log in (SAVE) and (RULES) Event Listener 
+
+// RESET Score
+
+function resetScore() {
+    userScore = 0;
+    compScore = 0;
+    compScore_span.innerHTML = "0";
+    userScore_span.innerHTML = "0";
+    result_p.innerHTML = "<p>Click & Play</p>";
+}
+button_reset.addEventListener("click", resetScore);
+button_rules.addEventListener("click", function () {
+    popup.style.display = "block";
+});
+button_close_rules.addEventListener("click", function () {
+    popup.style.display = "none";
+});
+button_save.addEventListener("click", function () {
+    save_popup.style.display = "block";
+});
+button_close_save.addEventListener("click", function () {
+    save_popup.style.display = "none";
+});
+submit_save.addEventListener("click", function () {
+    const name = document.getElementById("name_field").value;
+    saveScore(name);
+});
+
+function winGame() {
+    if(userScore>compScore) {
+        alert("You won! Save your score and play again!");
+    }
+    save_popup.style.display = "block";
+}
+
+function loseGame() {
+    if(compScore>userScore) {
+        alert("You lost! Try again!");
+    }
+    resetScore();
+}
+
+// Game User / Computer & User Choices / 
+
+function game(userChoice) {
+    const compChoice = getCompChoice();
+    switch (userChoice + compChoice) {
+
+        case "rockscissors":
+        case "rocklizard":
+        case "paperrock":
+        case "paperspock":
+        case "scissorspaper":
+        case "scissorslizard":
+        case "lizardspock":
+        case "lizardpaper":
+        case "spockrock":
+        case "spockscissors":
+
+            WIN(userChoice, compChoice);
+            break;
+
+        case "rockpaper":
+        case "paperscissors":
+        case "paperlizard":
+        case "scissorsrock":
+        case "scissorsspock":
+        case "lizardrock":
+        case "lizardscissors":
+        case "rockspock":
+        case "spockpaper":
+        case "spocklizard":
+
+            LOST(userChoice, compChoice);
+            break;
+
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+        case "lizardlizard":
+        case "spockspock":
+
+            DRAW(userChoice, compChoice);
+            break;
+    }
+    if(userScore >= 10) {
+        winGame();
+    }
+    if(compScore == 10) {
+        loseGame();
+    }
+}
+
+// Event listeners to game area buttons
+function main() {
+
+    rock_div.addEventListener("click", function () {
+        game("rock");
+    });
+    paper_div.addEventListener("click", function () {
+        game("paper");
+    });
+    scissors_div.addEventListener("click", function () {
+        game("scissors");
+    });
+    lizard_div.addEventListener("click", function () {
+        game("lizard");
+    });
+    spock_div.addEventListener("click", function () {
+        game("spock");
+    });
+
+}
+
+main();
+renderScore();
+
